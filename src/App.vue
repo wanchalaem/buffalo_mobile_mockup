29015236be279ea89187f3e27022fc61c0c00c5f
wanchalaem <wanchalaem_dodo@hotmail.com>
2020-06-19 13:08:24 +0700

    create UI

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="app">
+   <v-app>
+     <v-main>
+       <router-view></router-view>
+     </v-main>
+   </v-app>
+ </div>
+</template>
+
+<script>
+ 
+
+export default {
+  name: 'App', 
+  data: () => ({
+    //
+  }),
+};
+</script>
